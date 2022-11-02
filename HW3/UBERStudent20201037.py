bf21d509d7f59c4355051404e6e4ede40f2acd68
CHOHEE2000 <ljdyjv@naver.com>
2022-11-02 11:20:45 +0900

    hw3_2

--- HW3/UBERStudent20201037.py
+++ HW3/UBERStudent20201037.py
@@ -6,35 +6,30 @@ uber_file = sys.argv[1]
 result_file = sys.argv[2]
 weekdayCode = ['MON','TUE','WED','THU','FRI','SAT','SUN']
 
-try:
-	f = open(uber_file, "rt")
-	output = dict()
-	while True:
-		row = f.readline().strip()
-		if not row: break
+f = open(uber_file, "rt")
+output = dict()
+while True:
+	row = f.readline().strip()
+	if not row: break
 
-		uberList = row.split(',')
-		date = uberList[1].split('/')
-		day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
+	uberList = row.split(',')
+	date = uberList[1].split('/')
+	day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
 
-		key = uberList[0]+","+weekdayCode[day]
-		val = [int(uberList[2]), int(uberList[3])]
+	key = uberList[0]+","+weekdayCode[day]
+	val = [int(uberList[2]), int(uberList[3])]
 
-		if key not in output:
-			output[key] = val
-		else:
-			valList = output.get(key)
-			valList[0] += val[0]
-			valList[1] += val[1]
-			output[key] = valList
+	if key not in output:
+		output[key] = val
+	else:
+		valList = output.get(key)
+		valList[0] += val[0]
+		valList[1] += val[1]
+		output[key] = valList
 
-	r = open(result_file, "wt")
-	for i, j in output.items():
-		s = "{} {}\n".format(i,j)
-		r.write(s)
+f = open(result_file, "wt")
+for i, j in output.items():
+	s = "{} {}\n".format(i,j)
+	f.write(s)
 
-except:
-	print("no file")
-finally:
-	f.close()
-	r.close()
+f.close()
