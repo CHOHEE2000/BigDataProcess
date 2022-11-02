0f0ee4631c6fe67633788f6fc62ab0a5e42d2124
CHOHEE2000 <ljdyjv@naver.com>
2022-11-02 11:12:05 +0900

    hw3_1

--- HW3/IMDBStudent20201037.py
+++ HW3/IMDBStudent20201037.py
@@ -4,31 +4,35 @@ import sys
 file_path = sys.argv[1]
 result_file = sys.argv[2]
 
-f = open(file_path, "rt") 
-genrelist = dict()
-while True:
-	row = f.readline().strip()
-	if not row: break
+try:
+	f = open(file_path, "rt") 
+	genrelist = dict()
+	while True:
+		row = f.readline().strip()
+		if not row: break
 
-	str = row.split('::')
-	if str[2].find('|') != -1:
-		genres = str[2].split('|')
-		for i in genres:
-			if i not in genrelist:
-				genrelist[i] = 1
-			else:
-				genrelist[i] += 1
-	else:
-		genres = str[2]
-		if genres not in genrelist:
-			genrelist[genres] = 1
+		str = row.split('::')
+		if str[2].find('|') != -1:
+			genres = str[2].split('|')
+			for i in genres:
+				if i not in genrelist:
+					genrelist[i] = 1
+				else:
+					genrelist[i] += 1
 		else:
-			genrelist[genres] += 1
+			genres = str[2]
+			if genres not in genrelist:
+				genrelist[genres] = 1
+			else:
+				genrelist[genres] += 1
 
-r = open(result_file, "wt")
-for i, j in genrelist.items():
-	s = "{} {}\n".format(i,j)
-	r.write(s)
+	r = open(result_file, "wt")
+	for i, j in genrelist.items():
+		s = "{} {}\n".format(i,j)
+		r.write(s)
+except:
+	print("no file")
 
-f.close()
-r.close()
+finally:
+	f.close()
+	r.close()
--- HW3/UBERStudent20201037.py
+++ HW3/UBERStudent20201037.py
@@ -6,31 +6,35 @@ uber_file = sys.argv[1]
 result_file = sys.argv[2]
 weekdayCode = ['MON','TUE','WED','THU','FRI','SAT','SUN']
 
-f = open(uber_file, "rt")
-output = dict()
-while True:
-	row = f.readline().strip()
-	if not row: break
+try:
+	f = open(uber_file, "rt")
+	output = dict()
+	while True:
+		row = f.readline().strip()
+		if not row: break
 
-	uberList = row.split(',')
-	date = uberList[1].split('/')
-	day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
+		uberList = row.split(',')
+		date = uberList[1].split('/')
+		day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
 
-	key = uberList[0]+","+weekdayCode[day]
-	val = [int(uberList[2]), int(uberList[3])]
+		key = uberList[0]+","+weekdayCode[day]
+		val = [int(uberList[2]), int(uberList[3])]
 
-	if key not in output:
-		output[key] = val
-	else:
-		valList = output.get(key)
-		valList[0] += val[0]
-		valList[1] += val[1]
-		output[key] = valList
+		if key not in output:
+			output[key] = val
+		else:
+			valList = output.get(key)
+			valList[0] += val[0]
+			valList[1] += val[1]
+			output[key] = valList
 
-r = open(result_file, "wt")
-for i, j in output.items():
-	s = "{} {}\n".format(i,j)
-	r.write(s)
+	r = open(result_file, "wt")
+	for i, j in output.items():
+		s = "{} {}\n".format(i,j)
+		r.write(s)
 
-f.close()
-r.close()
+except:
+	print("no file")
+finally:
+	f.close()
+	r.close()
